0f1d698fc5e4440181c9efdb3a468fa07da585b2
ALEXANDER POROKHIN (workplace) <softoad@yandex.ru>
2021-02-15 14:04:48 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.20"
+}
+
+group = "roslesingorg.porokhin"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
