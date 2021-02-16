febf05ec844d239502ba2706e58600f6b828c988
ALEXANDER POROKHIN (workplace) <softoad@yandex.ru>
2021-02-16 17:20:39 +0300

    SomeFileReader created

--- src/main/kotlin/FileComparator.kt
+++ src/main/kotlin/FileComparator.kt
@@ -8,11 +8,31 @@ import java.util.concurrent.ArrayBlockingQueue
 class FileComparator(private val file1: File, private val file2: File, private val visualCapture: Int = 15, private val bufferSize: Int = 100) {
 
     fun compare(){
-        val f1 = if (file1.length() >= file2.length()) file1 else file2
-        val f2 = if (f1 == file1) file2 else file1
-        val reader1 = BufferedReader(FileReader(f1))
-        val reader2 = BufferedReader(FileReader(f2))
-        val bufferBefore = ArrayBlockingQueue<String>(visualCapture)
+        val reader = SomeFileReader(file1, file2, bufferSize)
+        var block = listOf("1" to "1")
+        while (block.isNotEmpty()){
+            block = reader.readBlock()
+            for (i in block.indices){
+                val first = block[i].first
+                val second = block[i].second
+                if (first == second) continue
+                if (first.equalLine(second)){
+
+                }
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+
+        /*val bufferBefore = ArrayBlockingQueue<String>(visualCapture)
         val comparedResult = mutableListOf<ComparedPair>()
         while (reader1.ready()){
             val s1 = reader1.readLine()
@@ -28,15 +48,40 @@ class FileComparator(private val file1: File, private val file2: File, private v
                 ComparedLine(s1, LineType.CHANGED)
             ))
 
-
-
-
-
-        }
-
+        }*/
     }
 
+
+
     private fun String.equalLine(other: String): Boolean{
+
+        fun String.equalForOffset(other: String, offset: Int): Int{
+            val start = substring(0, offset)
+            val end = substring(lastIndex - offset, length)
+            val idx = lastIndex/2
+            val middle = substring(idx, offset + idx)
+            val piece1 = substring(3, offset + 3)
+            val arr1 = this.toCharArray()
+            val arr2 = other.toCharArray()
+            val b1 = other.contains(start)
+            val b2 = other.contains(end)
+            val b3 = other.contains(middle)
+            val b4 = arr1.subtract(arr2.asIterable()).size < length / 2
+            val b5 = other.startsWith(start)
+            val b6 = other.endsWith(end)
+            val b7 = other.contains(piece1)
+            var bCounter = 0
+            if (b1) bCounter++
+            if (b2) bCounter++
+            if (b3) bCounter++
+            if (b4) bCounter++
+            if (b5) bCounter++
+            if (b6) bCounter++
+            if (b7) bCounter++
+            println("$b1  $b2  $b3  $b4  $b5  $b6  $b7")
+            return bCounter
+        }
+
         var run = 1
         var sum = this.equalForOffset(other, 3)
         if (length > 10 && other.length > 10){
@@ -49,32 +94,7 @@ class FileComparator(private val file1: File, private val file2: File, private v
         return res >= 50
 
     }
-    private fun String.equalForOffset(other: String, offset: Int): Int{
-        val start = substring(0, offset)
-        val end = substring(lastIndex - offset, length)
-        val idx = lastIndex/2
-        val middle = substring(idx, offset + idx)
-        val piece1 = substring(3, offset + 3)
-        val arr1 = this.toCharArray()
-        val arr2 = other.toCharArray()
-        val b1 = other.contains(start)
-        val b2 = other.contains(end)
-        val b3 = other.contains(middle)
-        val b4 = arr1.subtract(arr2.asIterable()).size < length / 2
-        val b5 = other.startsWith(start)
-        val b6 = other.endsWith(end)
-        val b7 = other.contains(piece1)
-        var bCounter = 0
-        if (b1) bCounter++
-        if (b2) bCounter++
-        if (b3) bCounter++
-        if (b4) bCounter++
-        if (b5) bCounter++
-        if (b6) bCounter++
-        if (b7) bCounter++
-        println("$b1  $b2  $b3  $b4  $b5  $b6  $b7")
-        return bCounter
-    }
+
 
 
 
---
+++ src/main/kotlin/SomeFileReader.kt
@@ -0,0 +1,36 @@
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+
+class SomeFileReader(file1: File, file2: File, private val bufferSize: Int) {
+    private var reader1 : BufferedReader
+    private var reader2 : BufferedReader
+    init {
+        val f1 = if (file1.length() >= file2.length()) file1 else file2
+        val f2 = if (f1 == file1) file2 else file1
+        reader1 = BufferedReader(FileReader(f1))
+        reader2 = BufferedReader(FileReader(f2))
+    }
+
+    fun readBlock(): List<Pair<String, String>>{
+        val result = mutableListOf<Pair<String, String>>()
+        for (i in 0..bufferSize){
+            when{
+                !reader1.ready() && reader2.ready() -> result.add("" to reader2.readLine())
+                reader1.ready() && !reader2.ready() -> result.add(reader1.readLine() to "")
+                !reader1.ready() && !reader2.ready() -> {
+                    close()
+                    return emptyList()
+                }
+                else -> result.add(reader1.readLine() to reader2.readLine())
+            }
+        }
+
+        return result
+    }
+
+    private fun close(){
+        reader1.close()
+        reader2.close()
+    }
+}
